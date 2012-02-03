8a758ab054be4a0ddddebf6afbc364255a0a4f4f
Vladimir Kirichenko <vladimir.kirichenko@gmail.com>
2012-02-03 21:04:12 +0200

    build script

--- build.sh
+++ build.sh
@@ -2,16 +2,18 @@
 rebar clean
 rebar eunit || exit -1
 rebar compile || exit -1
-mkdir -p out
+mkdir -p build
+mkdir -p dist
 
 MODULES="common leveldb yaws csv eunit io"
 
 for m in $MODULES
 do
-    MODOUT=out/strikead_$m
+    MODOUT=build/strikead_$m
     mkdir -p $MODOUT
 
     cp -v -r $m/ebin $MODOUT
     [ -d $m/include ] && cp -v -r $m/include $MODOUT
 done
-exit 0
+
+tar -czf dist/strikead-erlang-commons.tar.gz -C build .
