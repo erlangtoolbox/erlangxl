49843478b72568dfac41ba7fb308226e9ddfe0e6
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2012-02-28 19:06:37 +0200

    - restructured

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
+rm -rf build dist
 rebar clean
 rebar eunit || exit -1
 rebar compile || exit -1
 mkdir -p build
 mkdir -p dist
 
-MODULES="common leveldb yaws csv eunit io"
+MODULES="strikead_stdlib strikead_leveldb strikead_yaws strikead_csv strikead_eunit strikead_io"
 
 for m in $MODULES
 do
-    MODOUT=build/strikead_$m
+    MODOUT=build/$m
     mkdir -p $MODOUT
 
     cp -v -r $m/ebin $MODOUT
