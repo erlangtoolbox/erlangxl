db127e2ad2fc9fded1eb204c678a433f942d4900
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2012-03-23 18:51:27 +0200

    - strikead_list:emap error handling map

--- strikead_stdlib/src/strikead_list.erl
+++ strikead_stdlib/src/strikead_list.erl
@@ -1,6 +1,6 @@
 -module(strikead_list).
 
--export([find/2, first/1]).
+-export([find/2, first/1, emap/2]).
 
 find(_Pred, []) -> not_found;
 find(Pred, [H|T]) ->
@@ -11,3 +11,14 @@ find(Pred, [H|T]) ->
 
 first([]) -> empty;
 first([H|_]) -> {ok, H}.
+
+emap(F, List) -> emap(F, [], List).
+
+emap(_F, Acc, []) -> {ok, lists:reverse(Acc)};
+emap(F, Acc, [H|T]) ->
+    case F(H) of
+        {ok, R} -> emap(F, [R|Acc], T);
+        X -> X
+    end.
+
+
