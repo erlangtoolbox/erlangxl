aaedf1ca796f73adb7d56ccf2584af221f022001
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2012-04-06 19:00:38 +0300

    - add .._lists: index element index - add .._lists: keypsort pattern sort

--- strikead_stdlib/src/strikead_lists.erl
+++ strikead_stdlib/src/strikead_lists.erl
@@ -1,37 +1,62 @@
 -module(strikead_lists).
 
--export([find/2, first/1, emap/2, mapfilter/2]).
+-export([find/2, first/1, emap/2, mapfilter/2, index/2, keypsort/3]).
 
--spec find/2 :: (fun((term()) -> boolean()), [term()]) -> strikead_maybe_m:monad(term()).
+-spec find/2 :: (fun((term()) -> boolean()), [term()])
+	-> strikead_maybe_m:monad(term()).
 find(_Pred, []) -> nothing;
 find(Pred, [H|T]) ->
-    case Pred(H) of
-        true -> {ok, H};
-        _ -> find(Pred, T)
-    end.
+	case Pred(H) of
+		true -> {ok, H};
+		_ -> find(Pred, T)
+	end.
 
 -spec first/1 :: ([term()]) -> strikead_maybe_m:monad(term()).
 first([]) -> nothing;
 first([H|_]) -> {ok, H}.
 
--spec emap/2 :: (fun((term()) -> error_m:monad(term())), [term()]) -> error_m:monad([term()]).
+-spec emap/2 :: (fun((term()) -> error_m:monad(term())), [term()])
+	-> error_m:monad([term()]).
 emap(F, List) -> emap(F, [], List).
 
--spec emap/3 :: (fun((term()) -> error_m:monad(term())), [term()], [term()]) -> error_m:monad([term()]).
+-spec emap/3 :: (fun((term()) -> error_m:monad(term())), [term()], [term()])
+	-> error_m:monad([term()]).
 emap(_F, Acc, []) -> {ok, lists:reverse(Acc)};
 emap(F, Acc, [H|T]) ->
-    case F(H) of
-        {ok, R} -> emap(F, [R|Acc], T);
-        X -> X
-    end.
+	case F(H) of
+		{ok, R} -> emap(F, [R|Acc], T);
+		X -> X
+	end.
 
 -spec mapfilter/2 :: (fun((term()) -> false | term()), [term()]) -> [term()].
 mapfilter(F, L) -> mapfilter([], F, L).
 
--spec mapfilter/3 :: ([term()], fun((term()) -> strikead_maybe_m:monad(term())), [term()]) -> [term()].
+-spec mapfilter/3 :: ([term()],
+	fun((term()) -> strikead_maybe_m:monad(term())), [term()]) -> [term()].
 mapfilter(Acc, _F, []) -> lists:reverse(Acc);
 mapfilter(Acc, F, [H | T]) ->
 	case F(H) of
 		nothing -> mapfilter(Acc, F, T);
 		{ok, X} -> mapfilter([X | Acc], F, T)
 	end.
+
+-spec keypsort/3 :: ([term()], integer(), [{term(), term()}])
+	-> [{term(), term()}].
+keypsort(Keys, N, L) ->
+	C = fun(A, B) ->
+		case {index(element(N, A), Keys), index(element(N, B), Keys)} of
+			{nothing, _} -> true;
+			{_, nothing} -> false;
+			{{ok, I1}, {ok, I2}} -> I1 =< I2
+		end
+	end,
+	lists:sort(C, L).
+
+-spec index/2 :: (term(), [term()]) -> strikead_maybe_m:monad(integer()).
+index(X, L) -> index(X, 1, L).
+
+-spec index/3 :: (term(), integer(), [term()])
+	-> strikead_maybe_m:monad(integer()).
+index(_X, _I, []) -> nothing;
+index(X, I, [X | _]) -> {ok, I};
+index(X, I, [_ | T]) -> index(X, I + 1, T).
--- strikead_stdlib/test/strikead_lists_tests.erl
+++ strikead_stdlib/test/strikead_lists_tests.erl
@@ -8,8 +8,19 @@ find_test() ->
     ?assertEqual(nothing, strikead_lists:find(fun(X) -> X == 0 end, L)).
 
 emap_test() ->
-    ?assertEqual({ok, [1,2,3]}, strikead_lists:emap(fun(X) -> X end, [{ok, 1}, {ok, 2}, {ok, 3}])),
-    ?assertEqual({error, nope}, strikead_lists:emap(fun(X) -> X end, [{ok, 1}, {error, nope}, {ok, 3}])).
+	L1 = [{ok, 1}, {ok, 2}, {ok, 3}],
+    ?assertEqual({ok, [1, 2, 3]},
+		strikead_lists:emap(fun(X) -> X end, L1)),
+	L2 = [{ok, 1}, {error, nope}, {ok, 3}],
+    ?assertEqual({error, nope},
+		strikead_lists:emap(fun(X) -> X end, L2)).
 
 mapfilter_test() ->
-	?assertEqual([4,16], strikead_lists:mapfilter(fun(X) when X rem 2 == 0 -> {ok, X * X}; (_) -> nothing end, [1,2,3,4,5])).
+	L = [1, 2, 3, 4, 5],
+	?assertEqual([4, 16],
+		strikead_lists:mapfilter(
+			fun(X) when X rem 2 == 0 -> {ok, X * X}; (_) -> nothing end, L)).
+
+keypsort_test() ->
+	?assertEqual([{z, 1},{x, 2},{y, 3}],
+		strikead_lists:keypsort([z,x,y], 1, [{x, 2},{z, 1},{y, 3}])).
