77217042cdbd3f28b976c29063f8e0c19584bc67
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2012-04-09 15:43:24 +0300

    - add url: escape, substitute, escape_params

--- strikead_net/src/strikead_url.erl
+++ strikead_net/src/strikead_url.erl
@@ -1,9 +1,20 @@
 -module(strikead_url).
 
--export([to_query/1]).
+-export([to_query/1, escape_params/1, escape/1, substitute/2]).
 
 -spec to_query/1 :: ([{atom(), term()}]) -> string().
 to_query(List) ->
-	string:join([atom_to_list(Key) ++ "=" ++
-	edoc_lib:escape_uri(strikead_string:to_string(Value))
-	|| {Key, Value} <- List ], "&").
+	string:join(
+		[atom_to_list(Key) ++ "=" ++ escape(Value) || {Key, Value} <- List],
+	"&").
+
+-spec escape_params/1 :: ([{atom(), term()}]) -> [{atom(), term()}].
+escape_params(List) ->
+	lists:map(fun({K, V}) -> {K, escape(V)} end, List).
+
+-spec escape/1 :: (term()) -> string().
+escape(V) -> edoc_lib:escape_uri(strikead_string:to_string(V)).
+
+-spec substitute/2 :: (string(), strikead_string:subst_map()) -> string().
+substitute(Url, List) ->
+	strikead_string:substitute(Url, escape_params(List)).
--- strikead_net/test/strikead_url_tests.erl
+++ strikead_net/test/strikead_url_tests.erl
@@ -5,3 +5,13 @@
 to_query_test() ->
 	?assertEqual("landing=http%3a%2f%2fstrikead.com&a=1",
 		strikead_url:to_query([{landing, "http://strikead.com"}, {a, 1}])).
+
+
+escape_params_test() ->
+	?assertEqual([{a,"a%2bb"}],
+		strikead_url:escape_params([{a,"a+b"}])).
+
+substitute_test() ->
+	?assertEqual("http://strikead.com?a=a%2bb",
+	strikead_url:substitute("http://strikead.com?a={a}",[{a, "a+b"}])).
+
