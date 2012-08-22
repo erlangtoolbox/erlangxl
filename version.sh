f56aeb226c39e8ebba7080f6c2dcc195e4eec941
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2012-08-22 13:49:51 +0300

    - json_binder_test eunit only - version X.Y.Z.%04d formatted

--- version.sh
+++ version.sh
@@ -1,7 +1,4 @@
 PREFIX=1.0.0
-if [ -z $BUILD_NUMBER ]
-then
-	echo -n $PREFIX
-else
-	echo -n $PREFIX.$BUILD_NUMBER
-fi
+[ -z $BUILD_NUMBER ] && BUILD_NUMER=0
+
+echo -n $PREFIX.`printf "%04d\n" $BUILD_NUMBER`
