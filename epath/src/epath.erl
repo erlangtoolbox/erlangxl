535df42822f16d8121761ec42d098d0edf2eb59d
Vladimir Kirichenko <vladimir.kirichenko@gmail.com>
2012-08-30 01:50:08 +0300

    - parametric epath:select, epath:update

--- epath/src/epath.erl
+++ epath/src/epath.erl
@@ -5,7 +5,10 @@
 -compile({parse_transform, do}).
 
 %% API
--export([select/2, update/3]).
+-export([select/2, select/3, update/3, update/4]).
+
+-spec select/3 :: (string(), list(), tuple() | list()) -> error_m:monad(option_m:monad(term())).
+select(EPath, Params, X) -> select(xl_string:format(EPath, Params), X).
 
 -spec select/2 :: (string(), tuple() | list()) -> error_m:monad(option_m:monad(term())).
 select(EPath, X) ->
@@ -33,6 +36,9 @@ predicate({Op, A1, A2}) ->
 argument({element, N}, X) when is_tuple(X), N =< size(X) -> element(N, X);
 argument({element, _}, _) -> undefined;
 argument({_, X}, _) -> X.
+
+-spec update/4 :: (string(), list(), any(), tuple() | list()) -> error_m:monad(term()).
+update(EPath, Params, R, X) -> update(xl_string:format(EPath, Params), R, X).
 
 -spec update/3 :: (string(), any(), tuple() | list()) -> error_m:monad(term()).
 update(EPath, R, X) ->
