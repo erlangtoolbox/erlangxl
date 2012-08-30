a1cb3302584110d40dca4520c8d70388b69a9cc3
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2012-08-30 17:59:22 +0300

    - ebt #31

--- xl_json/src/xl_json_ebt_bindc.erl
+++ xl_json/src/xl_json_ebt_bindc.erl
@@ -1,15 +1,11 @@
 -module(xl_json_ebt_bindc).
 
--compile({parse_transform, do}).
-
 -behaviour(ebt_task).
 
--export([perform/2]).
+-export([perform/3]).
 
-perform(Dir, _Config) ->
-    do([error_m ||
-        Bindings <- return(filelib:wildcard(Dir ++ "/src/*.bind")),
-        xl_lists:eforeach(fun(File) ->
-            xl_json_bindc:compile(File, Dir)
-        end, Bindings)
-    ]).
+perform(_Target, Dir, _Config) ->
+    Bindings = filelib:wildcard(Dir ++ "/src/*.bind"),
+    xl_lists:eforeach(fun(File) ->
+        xl_json_bindc:compile(File, Dir)
+    end, Bindings).
