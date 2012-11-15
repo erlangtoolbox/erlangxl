09652fbe1abff01b965933d43aa4f0ff94957fcc
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2012-11-15 18:06:38 +0200

    xl_application:get_env with default

--- xl_stdlib/src/xl_application.erl
+++ xl_stdlib/src/xl_application.erl
@@ -3,8 +3,15 @@
 -author("volodymyr.kyrychenko@strikead.com").
 
 %% API
--export([eget_env/2]).
+-export([eget_env/2, get_env/3]).
 
--spec eget_env/2 :: (atom(), atom()) -> error_m:monad(any()).
+-spec(eget_env(atom(), atom()) -> error_m:monad(any())).
 eget_env(Application, Env) ->
     option_m:to_error_m(application:get_env(Application, Env), {undefined, {Application, Env}}).
+
+-spec(get_env(atom(), atom(), term()) -> term()).
+get_env(Application, Env, Default) ->
+    case application:get_env(Application, Env) of
+        {ok, Val} -> Val;
+        _ -> Default
+    end.
