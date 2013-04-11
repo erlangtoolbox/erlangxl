90830ffb0e0961ab4396ec964e5e50bc8240069b
Vladimir Kirichenko <vladimir.kirichenko@gmail.com>
2013-04-11 16:52:37 +0300

    xl_lists:kvmerge

--- xl_stdlib/test/xl_lists_tests.erl
+++ xl_stdlib/test/xl_lists_tests.erl
@@ -190,4 +190,4 @@ nth_test() ->
     ?assertEqual(undefined, xl_lists:nth(22, [1, 2, 3])).
 
 kvmerge_test() ->
-    ?_assertEqual([{a, 2}, {b, 4}], xl_lists:kvmerge(fun erlang:'+'/2, [{b, 1}], [{a, 2}, {b, 3}])).
+    ?assertEqual([{a, 2}, {b, 4}], xl_lists:kvmerge(fun erlang:'+'/2, [{a, 2}, {b, 3}], [{b, 1}])).
