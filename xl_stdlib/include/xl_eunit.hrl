d1ddfabecc4da3255441b2a53e94f4ea7b562692
Dmitry Kasimtsev <dmitry.kasimtsev@strikead.com>
2013-10-08 17:04:59 +0300

    formatted out

--- xl_stdlib/include/xl_eunit.hrl
+++ xl_stdlib/include/xl_eunit.hrl
@@ -4,8 +4,7 @@
 
 -define(assertFilesEqual(File1, File2), begin
     ?assertEquals({ok, true}, xl_file:exists(File1)),
-    ?assertEquals({ok, true}, xl_file:exists(File2)),
-    ?assertEquals(xl_file:read_file(File1), xl_file:read_file(File2))
+    ?assertEquals({ok, true}, xl_file:exists(File2)), ?assertEquals(xl_file:read_file(File1), xl_file:read_file(File2))
 end).
 
 -define(assertOk(Expr),
@@ -24,7 +23,7 @@ end).
         case X of
             Expect -> ok;
             Value ->
-                xl_eunit:format("\033[31m\033[1mFAILED\033[m ~s (~p)~nexpression: ~s~nexpected: ~9000p~n but was:\033[31m ~9000p~n\033[m", [?FILE, ?LINE, ??Expr, Expect, Value]),
+                xl_eunit:format("\033[31m\033[1mFAILED\033[m ~s (~p)~nexpression: ~s~nexpected: ~9000p~n but was:\033[31m ~9000p~n\033[m", [?FILE, ?LINE, ??Expr, io_lib:format("~p", [Expect]), io_lib:format("~p", [Value])]),
                 erlang:error(assertEquals_failed)
         end
     end)(Expr))
